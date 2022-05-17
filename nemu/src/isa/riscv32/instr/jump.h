c465b02e179786ef71c9baca49e2860d5d676b4c
tracer-ics2021 <tracer@njuics.org>
2022-05-17 23:01:53 +0800

    >  compile 201220000    Static hostname: VM-12-5-ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 8653f1c364544a1f86ffc83c1f0fee54            Boot ID: ace43d11964c434b9806d836ce178d40     Virtualization: kvm   Operating System: Ubuntu 20.04 LTS             Kernel: Linux 5.4.0-96-generic       Architecture: x86-64  23:01:53 up 4 days, 12:26,  0 users,  load average: 0.10, 0.06, 0.02

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -2,3 +2,9 @@ def_EHelper(jal) {
     rtl_addi(s, ddest, &s->pc, 4);
     rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
 }
+
+def_EHelper(jalr) {
+    rtl_addi(s, ddest, &s->pc, 4);
+    rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
+    rtl_andi(s, &s->dnpc, &s->dnpc, !1);
+}
