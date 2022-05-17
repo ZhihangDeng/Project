338c584c6d7b46682e74dc1b265141f8b352aae1
tracer-ics2021 <tracer@njuics.org>
2022-05-17 21:38:12 +0800

    >  compile 201220000    Static hostname: VM-12-5-ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 8653f1c364544a1f86ffc83c1f0fee54            Boot ID: ace43d11964c434b9806d836ce178d40     Virtualization: kvm   Operating System: Ubuntu 20.04 LTS             Kernel: Linux 5.4.0-96-generic       Architecture: x86-64  21:38:12 up 4 days, 11:02,  0 users,  load average: 0.10, 0.06, 0.02

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -74,7 +74,7 @@ static def_DHelper(J) {
   decode_op_r(s, id_dest, s->isa.instr.j.rd, true);
   sword_t simm = (s->isa.instr.j.imm20 << 20) | (s->isa.instr.j.imm10_1 << 10) | (s->isa.instr.j.imm11 << 9) | (s->isa.instr.j.imm19_12 << 1);
   simm = (simm << 12) >> 12;
-  decode_op_i(s, id_src2, simm, false);
+  decode_op_i(s, id_src1, simm, false);
 }
 
 //static inline int table_load (Decode *s)
--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -1,4 +1,4 @@
 def_EHelper(jal) {
     rtl_addi(s, ddest, &s->pc, 4);
-    rtl_addi(s, &s->snpc, &s->pc, id_src2->imm);
+    rtl_addi(s, &s->snpc, &s->pc, id_src1->imm);
 }
