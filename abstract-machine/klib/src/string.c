be883f31fd8f20dd6fd7ddbda2f3ecd5f6124408
tracer-ics2021 <tracer@njuics.org>
2022-05-24 21:25:23 +0800

    >  compile 201220000    Static hostname: VM-12-5-ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 8653f1c364544a1f86ffc83c1f0fee54            Boot ID: ace43d11964c434b9806d836ce178d40     Virtualization: kvm   Operating System: Ubuntu 20.04 LTS             Kernel: Linux 5.4.0-96-generic       Architecture: x86-64  21:25:23 up 11 days, 10:49,  3 users,  load average: 0.37, 0.09, 0.03

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,8 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  while((*dst++ = *src++) != '\0');
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,12 +18,31 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  while(1);
-  //panic("Not implemented");
+  char *tmp = dst;
+  while(*dst)
+    dst++;
+  while((*dst++ = *src++)!='\0');
+  return tmp;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  while(1);
+  int i = 0;
+  size_t len_s1 = sizeof(s1)/sizeof(char), len_s2 = sizeof(s2)/sizeof(char);
+  while((i < len_s1) && (i < len_s2)){
+    if(s1[i] > s2[i]){
+      return 1;
+    }else if(s1[i] < s2[i]){
+      return -1;
+    }
+    i++;
+  }
+  if(i == len_s1 && i == len_s2){
+    return 0;
+  }else if(i == len_s2){
+    return 1;
+  }else{
+    return -1;
+  }
   //panic("Not implemented");
 }
 
@@ -31,7 +51,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for(size_t i = 0; i < n; i++){
+    *(char*)(s+i) = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -39,8 +62,10 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  while(1);
-  //panic("Not implemented");
+  for(size_t i = 0; i < n; i++){
+    *(char*)(out+i) = *(char*)(in+i);
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
